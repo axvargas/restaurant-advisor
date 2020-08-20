a3638f17186af31d5546b527b70e530caba1b874
Andres X. Vargas <axvargas@fiec.espol.edu.ec>
2020-08-20 02:36:17 -0500

    Scafolding created with all screens

---
+++ app/screens/restaurants/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Restaurants = () => {
+    return (
+        <View>
+            <Text>Restaurants</Text>
+        </View>
+    )
+}
+export default Restaurants;
